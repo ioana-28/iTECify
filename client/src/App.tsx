7a6e94acf21c69223ea01ecb69715d74879e6d38
Ioana Barsan <ioana.barsan06@e-uvt.ro>
2026-03-28 18:27:13 +0200

    feat: add @types/react-router-dom and update imports in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import type { ReactElement } from "react";
-import { Navigate, Route, Routes, useLocation } from "react-router-dom";
+import { Navigate, Route, Routes, useLocation, Link } from "react-router-dom";
 import "./App.css";
 import { EditorPage } from "./pages/EditorPage";
 import { HealthPage } from "./pages/HealthPage";
